58f565545ef7b88af683ac17640676b5181adee0
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-29 18:28:58 -0400

    pass data

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 
 const MealDetailScreen = (props) => {
+
     return ( 
         <View style={styles.screen}>
             <Text>
