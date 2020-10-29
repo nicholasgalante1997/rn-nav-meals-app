fe8aa1d2e958e2ace94034d0a6d0748f0f9cf4a8
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-29 19:05:06 -0400

    header-button

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -10,7 +10,11 @@ const CategoryMealScreen = (props) => {
         return (
            <MealItem 
            title={itemData.item.title} 
-           onSelectMeal={() => {}} 
+           onSelectMeal={() => {
+               props.navigation.navigate({routeName: 'MealDetail', params: {
+                   mealId: itemData.item.id
+               }})
+           }} 
            duration={itemData.item.duration} 
            complexity={itemData.item.complexity} 
            affordability={itemData.item.affordability} 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
+import {MEALS} from '../data/dummy-data'
+import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 
 const MealDetailScreen = (props) => {
+
+    const mealId = props.navigation.getParam('mealId')
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
     return ( 
         <View style={styles.screen}>
             <Text>
-                The Meal Details Screen!
+                {selectedMeal.title}
             </Text>
+            <Button 
+                title="Go Back to Categories" 
+                onPress={() => {
+                    props.navigation.popToTop();
+                }}
+            />
         </View>
      );
+}
+
+MealDetailScreen.navigationOptions = navigationData => {
+   const mealId = navigationData.navigation.getParam('mealId')
+   const selectedMeal = MEALS.find(meal => meal.id === mealId)
+   return {
+       headerTitle: selectedMeal.title,
+       headerRight: (
+       <HeaderButtons>
+           <Item iconName='ios-star' 
+           onPress={() => {
+               console.log('Mark as Favorite')
+               }} 
+            />
+       </HeaderButtons>
+       )
+   }
 }
 
 const styles = StyleSheet.create({
