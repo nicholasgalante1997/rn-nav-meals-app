bdce0c69d3d5bf2a0e1e286f0260d24272466f2e
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-31 14:11:38 -0400

    sect5

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 import {createAppContainer} from 'react-navigation';
 import {Platform} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
 
 import Colors from '../constants/Colors'
 
 import CategoriesScreen from '../screens/CategoriesScreen'
 import CategoryMealsScreen from '../screens/CategoryMealsScreen'
 import MealDetailScreen from '../screens/MealDetailScreen'
+import FavoritesScreen from '../screens/FavoriteScreen'
 
 
 const MealsNavigator = createStackNavigator({
@@ -31,4 +35,34 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {
+        screen: MealsNavigator,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons 
+                name='ios-restaurant' 
+                size={25} 
+                color={tabInfo.tintColor} />
+            } 
+        }
+    },
+    Favorites: {
+        screen: FavoritesScreen,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons 
+                name='ios-star' 
+                size={25} 
+                color={tabInfo.tintColor} />
+            } 
+        }
+    }
+}, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor,
+        labelPosition: 'below-icon'
+    }
+});
+
+export default createAppContainer(MealsFavTabNavigator);
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native'
 import {MEALS} from '../data/dummy-data'
 import {HeaderButtons, Item} from 'react-navigation-header-buttons'
+import HeaderButton from '../components/HeaderButton'
 
 const MealDetailScreen = (props) => {
 
@@ -28,8 +29,8 @@ MealDetailScreen.navigationOptions = navigationData => {
    const selectedMeal = MEALS.find(meal => meal.id === mealId)
    return {
        headerTitle: selectedMeal.title,
-       headerRight: (
-       <HeaderButtons>
+       headerRight: () => (
+       <HeaderButtons HeaderButtonComponent={HeaderButton}>
            <Item iconName='ios-star' 
            onPress={() => {
                console.log('Mark as Favorite')
